3e4aa99bb4e871a2c947e3125cae4585cf4bc321
Ben Denton <ben@bendenton.com>
2018-05-18 13:35:54 -0700

    Updated scripts

--- crackme3_solve.py
+++ crackme3_solve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-# Author: Ben Denton (ben@bendenton.com) | 16 May 2018
+# Author: Ben Denton (ben@bendenton.com)
 # Solves "crackme3" from https://github.com/holbertonschool/dont_hate_the_hacker_hate_the_code
 
 # run ./crackme "$(<crackme_solution)" after executing this script to  verify
@@ -10,13 +10,13 @@ import angr
 import claripy
 
 def main():
-    target = "./crackme3"
-    proj = angr.Project(target)
+    proj = angr.Project("./crackme3")
 
-    argv1 = claripy.BVS("argv1", 4 * 8) # declare argv[1], as a symbolic bit vector.  This is the password input from the user.
-                                        # the strlen checks if the password == 4, set the BV to 4 bytes
+    # Declare argv[1], as a symbolic bit vector.  This is the password input from the user.
+    # Strlen checks if the password == 4, set the BV to 4 bytes (4 * 8 bits)
+    argv1 = claripy.BVS("argv1", 4 * 8)
 
-    initial_state = proj.factory.entry_state(args=[target, argv1])
+    initial_state = proj.factory.entry_state(args=["./crackme3", argv1])
 
     sm = proj.factory.simulation_manager(initial_state)
 
@@ -24,10 +24,8 @@ def main():
     avoid_addr = [0x4006f5, 0x40062b]
 
     sm.explore(find=find_addr, avoid=avoid_addr)
-    found = sm.found[0]
 
-    solution = found.solver.eval(argv1, cast_to=str)
-
+    solution = sm.found[0].solver.eval(argv1, cast_to=str)
     f=open("crackme3_solution","wb")
     f.write(solution)
     f.close()
--- crackme_solve.py
+++ crackme_solve.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-# Author: Ben Denton (ben@bendenton.com) | 17 April 2018
+# Author: Ben Denton (ben@bendenton.com)
 # Solves "crackme" from https://github.com/holbertonschool/dont_hate_the_hacker_hate_the_code
 
 # run ./crackme "$(<crackme_solution)" after executing this script to  verify
@@ -11,36 +11,46 @@ import claripy
 
 def main():
     proj = angr.Project('./crackme')
+    # pick a max length for input.
+    # 0x20 is the minimum length. i've no idea why.
+    input_size = 0x35;
 
-    input_size = 0x25; # Pick a max length for input. 0x20 is the minimum length. I've no idea why.
+    # declare argv[1], as a symbolic bit vector.
+    # this is the password input from the user.
+    argv1 = claripy.BVS('argv1', input_size * 8)
 
-    argv1 = claripy.BVS("argv1", input_size * 8) # declare argv[1], as a symbolic bit vector.  This is the password input from the user.
+    initial_state = proj.factory.entry_state(args=['./crackme', argv1])
 
-    initial_state = proj.factory.entry_state(args=["./crackme", argv1])
-
-    # For some reason if you constrain too few bytes, the solution isn't found. To be safe, I'm constraining them all.
+    # constrain 'password' to terminal friendly, printable character
     for byte in argv1.chop(8):
-        initial_state.add_constraints(byte != '\x00') # null
-        initial_state.add_constraints(byte != '\x60') # `
-        initial_state.add_constraints(byte != '\x5C') # \
-        initial_state.add_constraints(byte >= 'A') # \x30
+        initial_state.add_constraints(byte >= '0') # \x30
         initial_state.add_constraints(byte <= 'z') # \x7A
-        # Source: https://www.juniper.net/documentation/en_US/idp5.1/topics/reference/general/intrusion-detection-prevention-custom-attack-object-extended-ascii.html
-        # Constrain to printable characters
+        initial_state.add_constraints(byte != '\x5C') # \
+        initial_state.add_constraints(byte != '\x60') # `
+        # initial_state.add_constraints(byte != '@')
+        # initial_state.add_constraints(byte != ';')
+        # initial_state.add_constraints(byte != ':')
+        # initial_state.add_constraints(byte != '<')
+        # initial_state.add_constraints(byte != '=')
+        # initial_state.add_constraints(byte != '>')
+        # initial_state.add_constraints(byte != '[')
+        # initial_state.add_constraints(byte != ']')
+        # initial_state.add_constraints(byte != '^')
+        # initial_state.add_constraints(byte != '_')
+        # initial_state.add_constraints(byte != '?')
 
+    # Initialize a Sim with the constrained entry_state
     sm = proj.factory.simulation_manager(initial_state)
 
+    # run the Sim looking for a successful path to the find= block
+    # ignore any paths that include the avoid= blocks
                   # 0x40061b = success message
     sm.explore(find=0x40061b, avoid=0x40060a)
                                   # 0x40060a = password failure
 
-    found = sm.found[0] # In our case, there's only one printable solution.
+    # Concretize the symbolic solution, save to a file, and return result
+    solution = sm.found[0].solver.eval(argv1, cast_to=str)
 
-    solution = found.solver.eval(argv1, cast_to=str)
-
-    f=open("crackme_solution","wb")
-    f.write(solution)
-    f.close()
     return solution
 
 if __name__ == '__main__':
